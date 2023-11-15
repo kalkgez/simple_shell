2fd5d91518ae507f6205b372b7bb08333c0c44f0
kalkidan Gezahegn <kalkgezahegn@gmail.com>
2023-11-15 13:45:36 -0800

    modfiled code

--- error_messages_2.c
+++ error_messages_2.c
@@ -1,143 +1,69 @@
 #include "shell.h"
 
 /**
- * strcat_cd - A function that concatenate the error message
+ * error_env - A function that generate the error message for environment
  * @datash: A pointer to the data shell struct
- * @msg: The messege to append
- * @error: The error string to build
- * @ver_str: The varion string
- * Return: A poiner to the concatenated error string
+ * Return: The error message string
  */
-char *strcat_cd(data_shell *datash, char *msg, char *error, char *ver_str)
-{
-	char *illegal_flag;
-
-	_strcpy(error, datash->av[0]);
-	_strcat(error, ": ");
-	_strcat(error, ver_str);
-	_strcat(error, ": ");
-	_strcat(error, datash->args[0]);
-	_strcat(error, msg);
-	if (datash->args[1][0] == '-')
-	{
-		illegal_flag = malloc(3);
-		illegal_flag[0] = '-';
-		illegal_flag[1] = datash->args[1][1];
-		illegal_flag[2] = '\0';
-		_strcat(error, illegal_flag);
-		free(illegal_flag);
-	}
-	else
-	{
-		_strcat(error, datash->args[1]);
-	}
-
-	_strcat(error, "\n");
-	_strcat(error, "\0");
-	return (error);
-}
-
-/**
- * error_get_cd - A function that generate the error message
- * @datash: A pointer to the data shell struct
- * Return: A pointer to the generated errr message
- */
-char *error_get_cd(data_shell *datash)
-{
-	int length, len_id;
-	char *error, *ver_str, *msg;
-
-	ver_str = aux_itoa(datash->counter);
-	if (datash->args[1][0] == '-')
-	{
-		msg = ": Illegal option ";
-		len_id = 2;
-	}
-	else
-	{
-		msg = ": can't cd to ";
-		len_id = _strlen(datash->args[1]);
-	}
-
-	length = _strlen(datash->av[0]) + _strlen(datash->args[0]);
-	length += _strlen(ver_str) + _strlen(msg) + len_id + 5;
-	error = malloc(sizeof(char) * (length + 1));
-
-	if (error == 0)
-	{
-		free(ver_str);
-		return (NULL);
-	}
-
-	error = strcat_cd(datash, msg, error, ver_str);
-
-	free(ver_str);
-
-	return (error);
-}
-
-/**
- * error_not_found - A function that generate the error message for cd command
- * @datash: A pointer to the data shell struct
- * Return: The error messege string
- */
-char *error_not_found(data_shell *datash)
+char *error_env(data_shell *datash)
 {
 	int length;
 	char *error;
-	char *ver_str;
+	char *version_string;
+	char *statusMessage;
 
-	ver_str = aux_itoa(datash->counter);
-	length = _strlen(datash->av[0]) + _strlen(ver_str);
-	length += _strlen(datash->args[0]) + 16;
+	version_string = aux_itoa(datash->counter);
+	statusMessage = ": Unable to add/remove from environment\n";
+	length = _strlen(datash->av[0]) + _strlen(version_string);
+	length += _strlen(datash->args[0]) + _strlen(statusMessage) + 4;
 	error = malloc(sizeof(char) * (length + 1));
 	if (error == 0)
 	{
 		free(error);
-		free(ver_str);
+		free(version_string);
 		return (NULL);
 	}
+
 	_strcpy(error, datash->av[0]);
 	_strcat(error, ": ");
-	_strcat(error, ver_str);
+	_strcat(error, version_string);
 	_strcat(error, ": ");
 	_strcat(error, datash->args[0]);
-	_strcat(error, ": not found\n");
+	_strcat(error, statusMessage);
 	_strcat(error, "\0");
-	free(ver_str);
+	free(version_string);
+
 	return (error);
 }
-
 /**
- * error_exit_shell - A function that generate the error messege
- * @datash: A pointer to the data shell struct
+ * error_path_126 - A function that generate the error message for permissions
+ * @datash: Pointer to the data shell struct
  *
- * Return: The error messege string
+ * Return: 
  */
-char *error_exit_shell(data_shell *datash)
+char *error_path_126(data_shell *datash)
 {
 	int length;
+	char *version_string;
 	char *error;
-	char *ver_str;
 
-	ver_str = aux_itoa(datash->counter);
-	length = _strlen(datash->av[0]) + _strlen(ver_str);
-	length += _strlen(datash->args[0]) + _strlen(datash->args[1]) + 23;
+	version_string = aux_itoa(datash->counter);
+	length = _strlen(datash->av[0]) + _strlen(version_string);
+	length += _strlen(datash->args[0]) + 24;
 	error = malloc(sizeof(char) * (length + 1));
 	if (error == 0)
 	{
-		free(ver_str);
+		free(error);
+		free(version_string);
 		return (NULL);
 	}
 	_strcpy(error, datash->av[0]);
 	_strcat(error, ": ");
-	_strcat(error, ver_str);
+	_strcat(error, version_string);
 	_strcat(error, ": ");
 	_strcat(error, datash->args[0]);
-	_strcat(error, ": Illegal number: ");
-	_strcat(error, datash->args[1]);
-	_strcat(error, "\n\0");
-	free(ver_str);
-
+	_strcat(error, ": Permission denied\n");
+	_strcat(error, "\0");
+	free(version_string);
 	return (error);
 }
